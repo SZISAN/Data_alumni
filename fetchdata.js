909a9dfe2649ef1013fc70678bb6add3e8d83474
SZISAN <szisansa@gmail.com>
2025-03-26 06:30:46 +0600

    Update and rename fetchdata.js to f

--- fetchdata.js
+++
@@ -1,8 +0,0 @@
-// Fetch data from Google Sheets
-fetch("https://spreadsheets.google.com/feeds/list/1fkd8E9TIgTWhXrddQJiloSafe9GThcpt9s9v2-6obbI/od6/public/values?alt=json")
-  .then(response => response.json())
-  .then(data => {
-    const alumniData = data.feed.entry;
-    displayAlumni(alumniData);  // Call the function to display data
-  })
-  .catch(error => console.error("Error fetching data: ", error));
